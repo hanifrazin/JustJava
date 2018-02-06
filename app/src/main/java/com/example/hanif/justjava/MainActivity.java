84fac9b91e747ecdaa6e388327d26495603bb79d
“Hanif <hanifrazin@gmail.com>
2018-02-06 16:14:08 +0700

    Menambahkan id pada button dan menggunakan setonclicklistener

--- app/src/main/java/com/example/hanif/justjava/MainActivity.java
+++ app/src/main/java/com/example/hanif/justjava/MainActivity.java
@@ -3,23 +3,35 @@ package com.example.hanif.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submitOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitOrder = findViewById(R.id.btn_submit_order);
+        submitOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = 3;
+                display(quantity);
+                displayprice(quantity*5);
+            }
+        });
     }
 
-    public void submitOrder(View view) {
-        int quantity = 3;
-        display(quantity);
-        displayprice(quantity*5);
-    }
+//    public void submitOrder(View view) {
+//        int quantity = 3;
+//        display(quantity);
+//        displayprice(quantity*5);
+//    }
 
     private void display(int number) {
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
